9143b9e778e1f6ab0ee6effb0a57183bd27742f1
Sikor08 <sikor08@gmail.com>
2023-09-05 20:59:36 +0300

    catalogHtml

--- latest.js
+++ latest.js
@@ -1,8 +1,58 @@
 const goodsArr = [];
 const latest = document.querySelector('.latestList')
 
-const getGood =fetch('latest.json').then(str => str.json()).then(data => data.forEach(good => {
-    goodsArr.push(good);
-})
-);
+const getGood = () => {
+    fetch('latest.json').then(str => str.json()).then(data => data.forEach(good => {
+        goodsArr.push(good);
+        renderGood(goodsArr)
+    
+    })
+    )
+}
 console.log(goodsArr)
+const renderGood = (arr) => {
+    latest.innerHTML = ''
+    arr.forEach(good => {
+        const latestGoodElem = document.createElement('li');
+        latestGoodElem.classList.add('latestGood')
+        const latestGoodImg = document.createElement('img');
+        latestGoodImg.setAttribute('src', good.img);
+        latestGoodImg.classList.add('latestGood__img')
+        latestGoodElem.append(latestGoodImg);
+
+        latest.append(latestGoodElem)
+
+        const latestGoodInfo = document.createElement('div');
+        latestGoodInfo.classList.add('latestGood__info')
+        latestGoodElem.append(latestGoodInfo)
+
+        const titleElem = document.createElement('h4');
+        titleElem.classList.add('latestGood__title')
+        titleElem.textContent = good.title;
+        latestGoodInfo.append(titleElem)
+
+        const textElem = document.createElement('p');
+        textElem.classList.add('latestGood__text');
+        textElem.textContent = good.text;
+        latestGoodInfo.append(textElem);
+
+        const priceCartWrap = document.createElement('div');
+        priceCartWrap.classList.add('priceCartWrap');
+        latestGoodInfo.append(priceCartWrap);
+
+        const latestGoodPriceElem = document.createElement('p');
+        latestGoodPriceElem.classList.add('latestGood__price');
+        latestGoodPriceElem.textContent = 'за 100 гр'
+        priceCartWrap.append(latestGoodPriceElem);
+
+        const priceValueElem = document.createElement('span');
+        priceValueElem.classList.add('latestGood__priceValue');
+        priceValueElem.textContent = '100р';
+        latestGoodPriceElem.prepend(priceValueElem);
+
+        const buyBtnImg = document.createElement('img');
+        buyBtnImg.setAttribute('src', 'img/cartGoodIcon.svg');
+        priceCartWrap.append(buyBtnImg)
+    })
+}
+getGood()
---
+++ promoSlider.js
@@ -0,0 +1,60 @@
+// swiper
+const sliderImgs = document.querySelectorAll('.sliderLine__img'),
+      sliderLine = document.querySelector('.sliderLine'),
+      sliderDots = document.querySelectorAll('.sliderDot'),
+      prevBtn = document.querySelector('.sliderBtn__prev'),
+      nextBtn = document.querySelector('.sliderBtn__next');
+
+    let sliderCount = 0;
+    let sliderWidth;
+//response
+    const showSlide = () => {
+        sliderWidth = document.querySelector('.slider').offsetWidth;
+        sliderLine.style.width = `${sliderWidth * sliderImgs.length}px`;
+        sliderImgs.forEach(item => item.style.width = sliderWidth + 'px');
+        // sliderImgs.forEach(item => item.style.height = 100 + '%');
+
+    }
+    window.addEventListener('resize', () => {
+        showSlide()
+    });
+    const rollSlide = () => {
+        sliderLine.style.transform = `translateX(${- sliderCount * sliderWidth}px)`
+    }
+    const nextSlide = () => {
+        sliderCount++;
+        sliderCount >= sliderImgs.length ? sliderCount = 0 : sliderCount;
+        rollSlide();
+        thisSlide(sliderCount);
+    }
+    const prevSlide = () => {
+        sliderCount--;
+        sliderCount < 0 ? sliderCount = sliderImgs.length - 1 : sliderCount;
+        rollSlide();
+        thisSlide(sliderCount);
+
+    }
+    nextBtn.addEventListener('click', () => {
+        nextSlide();
+    })
+    prevBtn.addEventListener('click', () => {
+        prevSlide()
+    })
+    const thisSlide = (index) => {
+        sliderDots.forEach(item => {item.classList.remove('sliderDot__activeDot');
+        sliderDots[index].classList.add('sliderDot__activeDot')
+
+        })
+    }
+    sliderDots.forEach((dot, index) => {
+        dot.addEventListener('click', () => {
+            sliderCount = index;
+            rollSlide();
+            thisSlide(sliderCount)
+        })
+    })
+    setInterval(() => {
+        nextSlide()
+    }, 2000);
+    
+    window.addEventListener('resize', showSlide());
