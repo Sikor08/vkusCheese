56d8f1ec941c70e4b0ea36b6abcecb0a9d16e0a7
Yury <92983534+Sikor08@users.noreply.github.com>
2024-02-07 11:56:15 +0300

    burger-menu fixed

--- js/burger.js
+++ js/burger.js
@@ -1,11 +1,19 @@
 const burgerBtn = document.querySelector('.burgerBtn'),
       contactsLink = document.querySelector('.headerNavLink__contacts'),
       burgerMenu = document.querySelector('.burgerMenu');
+      const body = document.querySelector('.body')
 burgerBtn.addEventListener('click', () => {
 burgerMenu.classList.toggle('burgerMenu__active');
 burgerBtn.classList.toggle('burgerBtn__active');
+if(burgerMenu.classList.contains('burgerMenu__active')) {
+    body.style.overflow = 'hidden'
+} else {
+    body.style.overflow = 'visible'
+
+}
 })
 contactsLink.addEventListener('click', () => {
     burgerMenu.classList.toggle('burgerMenu__active');
     burgerBtn.classList.toggle('burgerBtn__active');
+
 })
